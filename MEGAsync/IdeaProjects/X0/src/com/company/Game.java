881fdd6c9e3c5a555d653030cf2963fe0ecf2d1e
0NTARI0 <dimaploxyta@gmail.com>
2016-02-29 00:39:52 +0200

    commit 1

---
+++ MEGAsync/IdeaProjects/X0/src/com/company/Game.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Game
+{
+	private static final String GAME_NAME = "XO";
+
+	public static void printGameName()
+	{
+		System.out.println("GAME: %s" + Game.GAME_NAME);
+	}
+
+}
